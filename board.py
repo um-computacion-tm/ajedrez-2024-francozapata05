ce75898b31851ef1f76f0bbcc22e0f26e8bf7a8e
francozapata05 <f.zapata@alumno.um.edu.ar>
2024-08-15 18:22:09 -0300

    Separando en archivos las distintas clases.

--- board.py
+++ board.py
@@ -1,4 +1,9 @@
-from piece import Rook, Knight, Bishop, King, Queen, Pawn
+from rook import Rook
+from knight import Knight
+from bishop import Bishop
+from king import King
+from queen import Queen
+from pawn import Pawn
 
 class Board:
     def __init__(self):
---
+++ king.py
@@ -0,0 +1,5 @@
+from piece import Piece
+
+class King(Piece):
+    def mover(self):
+        pass
